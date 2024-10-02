f36e26905ef38db80f34b7af11a084ac87b1245e
Mattdoek10 <152287577+Mattdoek10@users.noreply.github.com>
2024-10-02 19:59:42 +0200

    Add files via upload

--- java.js
+++ java.js
@@ -1,9 +1,8 @@
 let deButton = document.querySelector("header button:last-of-type");
 let deNav = document.querySelector("nav")
-// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+
 deButton.onclick = toggleMenu;
 
-// stap 3: voeg in de functie een class toe aan de nav
 function toggleMenu() {
   deNav.classList.toggle("toonMenu");
 }
